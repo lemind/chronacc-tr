f509d6a03165dfc06e1ad38f113f723b3b5319e5
Dimitry Lyomin <leminds@gmail.com>
2019-02-17 15:25:29 +0300

    Cache projects

--- src/app.js
+++ src/app.js
@@ -1,15 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore, applyMiddleware, compose } from 'redux'
+
 import { Provider } from 'react-redux'
-import { composeWithDevTools } from 'redux-devtools-extension'
 
-import { createEpicMiddleware } from 'redux-observable'
-import { combineReducers } from 'redux'
-// persist
-import { PersistGate } from 'redux-persist/integration/react'
-
-import { store, persistor } from 'src/redux/store'
+import { store } from 'src/redux/store'
 
 import styles from './app.less'
 
@@ -18,9 +12,7 @@ import Root from './components/routing/Root'
 const renderApp = () => (
   render(
     <Provider store={ store }>
-      <PersistGate loading={ null } persistor={ persistor }>
-        <Root />
-      </PersistGate>
+      <Root />
     </Provider>,
     document.getElementById('app')
   )
