a61315399ebb3f518da9fae6e7c37752268c81a6
Dimitry Lyomin <leminds@gmail.com>
2020-02-08 20:50:24 +0300

    Add useCases draft

--- client/src/components/home/Starter/Starter.js
+++ client/src/components/home/Starter/Starter.js
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import withCases from 'helpers/withCases'
+import useCasesDraft from 'helpers/useCases'
 import TasksCases from 'cases/tasks'
 import Timer from 'components/home/Timer/Timer'
 import Tasks from 'components/home/Tasks/Tasks'
 
+// DoTo: deprecated
 @withCases(TasksCases)
-export default class Starter extends React.Component {
+export class StarterDeprecated extends React.Component {
   componentWillMount(){
     this.props.tasksCases.load({reset: true})
   }
@@ -29,3 +31,25 @@ export default class Starter extends React.Component {
   }
 }
 
+export default function Starter(){
+  const { tasksCases, tasks } = useCasesDraft(TasksCases)
+
+  useEffect(() => {
+    tasksCases && tasksCases.load && tasksCases.load({reset: true})
+
+    return () => {
+      tasksCases && tasksCases.unsubscribe()
+    }
+  }, [tasksCases])
+
+  const tasksList = tasks ? tasks.list : []
+
+  return (
+    <div>
+      <div>
+        <Timer />
+      </div>
+      <Tasks tasks={ tasksList } />
+    </div>
+  )
+}
