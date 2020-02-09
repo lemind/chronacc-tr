660d045c42762ab354b16032f6658a214010f017
Dimitry Lyomin <leminds@gmail.com>
2020-02-09 09:06:09 +0300

    Shift to fn cmps

--- client/src/components/common/blocks/Header/Header.js
+++ client/src/components/common/blocks/Header/Header.js
@@ -3,20 +3,11 @@ import React from 'react'
 import Menu from 'components/common/blocks/Menu/Menu'
 import Notifier from 'components/common/elements/Notifier/Notifier'
 
-export default class Header extends React.Component {
-  render() {
-
-    const notifierStyle = {
-      position: 'absolute',
-      top: 0,
-      right: '5px',
-    }
-
-    return (
-      <header>
-        <Menu />
-        <div style={ notifierStyle } ><Notifier /></div>
-      </header>
-    )
-  }
+export default function Header() {
+  return (
+    <header>
+      <Menu />
+      <Notifier />
+    </header>
+  )
 }
--- client/src/components/common/blocks/Menu/Menu.js
+++ client/src/components/common/blocks/Menu/Menu.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import { Link } from "react-router-dom"
 
-export default class Menu extends React.Component {
-
-  render() {
-    return (
-      <div>
-        <span><Link to="/">Home</Link></span>
-        <span> | </span>
-        <span><Link to="/projects">Projects</Link></span>
-      </div>
-    )
-  }
+export default function Menu() {
+  return (
+    <div>
+      <span><Link to="/">Home</Link></span>
+      <span> | </span>
+      <span><Link to="/projects">Projects</Link></span>
+    </div>
+  )
 }
