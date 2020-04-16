d084d8440c5995b8e3a3111b025e53bb4d25ba6a
Dimitry Lyomin <leminds@gmail.com>
2020-04-16 08:45:13 +0300

    Concretize project response type

--- client/src/api/projects.ts
+++ client/src/api/projects.ts
@@ -1,12 +1,29 @@
 import { ajax } from 'rxjs/ajax'
 import { API_HOST, HEADERS } from '../config'
 import { ProjectActionType } from 'src/redux/projects';
+import { IProject } from 'models/Project';
 
-type ResonseType = any
+// ToDo move to api/index
+export type TBackDataVersion = {
+  _v: number
+}
+export type TBackDataError = {
+  id: number,
+  message: string
+}
+
+
+type TProjectRaw = IProject & TBackDataVersion
+
+type TResponse = {
+  result: TProjectRaw[], // not required
+  error: TBackDataError, // not required
+  success: boolean
+}
 
 export const projectsAPI = {
   fetchProjects: () => {
-    return ajax.getJSON<ResonseType>(`${API_HOST}/projects`);
+    return ajax.getJSON<TResponse>(`${API_HOST}/projects`);
   },
   createProject: (action: ProjectActionType) => {
     return ajax.post(
